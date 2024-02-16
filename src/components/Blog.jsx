401e7c0e3c26b9c7c7201abdbda329ccbcd80244
akosdabasi <dabasi.akos@gmail.com>
2024-02-16 17:27:02 +0100

    refactoring Blog component

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -3,25 +3,21 @@ import './Blog.css'
 const Blog = ({ blog, handleOnView, handleLike, handleDelete, deletable }) =>
 {
   
-  return blog.view ? 
-  <div className="blogItem detailed">
-    <span>
-      {`${blog.title} by ${blog.author} `} 
-      <button onClick={() => handleOnView(blog.id)}>hide</button>
-      {deletable && <button className='deleteButton' onClick={() => handleDelete(blog.id)}>delete</button>}
-    </span>
-    <div>{blog.url}</div>
-    <div>{blog.likes} <button onClick={() => handleLike(blog)}>like</button></div>
-    <div>{`added by: ${blog.user.username}`}</div>
-  </div>  
-  :
-  <div className="blogItem">
-    <span>
-      {`${blog.title} by ${blog.author} `} 
-      <button onClick={() => handleOnView(blog.id)}>view</button>
-      {deletable && <button className='deleteButton' onClick={() => handleDelete(blog.id)}>delete</button>}
-    </span>
-  </div>  
-}
+  return (
+    <div className="blogItem">
+      <span>
+        {`${blog.title} by ${blog.author} `} 
+        <button onClick={() => handleOnView(blog.id)}>{blog.view ? 'hide' : 'view'}</button>
+        {deletable && <button className='deleteButton' onClick={() => handleDelete(blog.id)}>delete</button>}
+      </span>
+      {blog.view && 
+        <>
+        <div>{blog.url}</div>
+        <div>{blog.likes} <button onClick={() => handleLike(blog)}>like</button></div>
+        <div>{`added by: ${blog.user.username}`}</div>
+        </>
+      }
+    </div>  
+)}
   
 export default Blog
